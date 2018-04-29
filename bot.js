79325d6b6edaa0e27fa2ab9883f15cb1c81fdc13
LuizCOkkie <38788957+LuizCOkkie@users.noreply.github.com>
2018-04-29 20:32:39 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,25 +7,25 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'lm+ping') {
-    	message.reply(':ping_pong: pong :ping_pong: ');
+    	message.reply(':ping_pong: pong :ping_pong:');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'lm+yeah') {
-    	message.reply('Também estou feliz! :stuck_out_tongue: ');
+    	message.reply('Também estou feliz! :stuck_out_tongue:');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'lm+tapa') {
-    	message.reply(':wave::skin-tone-1: aí, seu tapa doeu :wave::skin-tone-1: ');
+    	message.reply(':wave::skin-tone-1: aí, seu tapa doeu :wave::skin-tone-1:');
   	}
 })
 
 client.on('message', message => {
     if (message.content === 'lm+smsgm') {
-    	message.reply('Bom dia!! Flor do dia! :blossom:  ');
+    	message.reply('Bom dia!! Flor do dia! :blossom:');
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
