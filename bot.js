c557384bac76a5f5482543def66a976179599233
LuizCOkkie <38788957+LuizCOkkie@users.noreply.github.com>
2018-04-29 15:15:17 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,22 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === 'lm+ping') {
     	message.reply('pong');
   	}
 });
+
+client.on('message', message => {
+    if (message.content === 'lm+yeah') {
+    	message.reply('Também estou feliz! :stuck_out_tongue: ');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'lm+tapa') {
+    	message.reply(':wave::skin-tone-1: aí, seu tapa doeu :wave::skin-tone-1: ');
+  	}
+})
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
